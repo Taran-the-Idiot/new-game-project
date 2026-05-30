e537cfc5c86eb95c2d610d96fd9c5d9572f76b0e
Taran Mittal <taranmittal09@gmail.com>
2026-05-30 23:22:59 +1000

    stuff

--- scripts/character_body.gd
+++ scripts/character_body.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 350.0
 const JUMP_VELOCITY = -1000.0
-
-
+var health = 10
+var health_frames = 60
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -23,3 +23,19 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func _process(delta: float) -> void:
+	health_frames -= 1
+	
+	
+	if health_frames < 0:
+		for body in get_node("Area2D").get_overlapping_bodies():
+			if body == get_parent().get_node("Enemy") or get_parent().get_node("Enemy2") or get_parent().get_node("Enemy3") or get_parent().get_node("Enemy4") or get_parent().get_node("Enemy5") or get_parent().get_node("Enemy6") or get_parent().get_node("Enemy7") or get_parent().get_node("Enemy8") or get_parent().get_node("Enemy9") or get_parent().get_node("Enemy10"):
+				health -= 1
+				health_frames = 60
+			else:
+				health = health
+		print(health)
+		print(get_node("Area2D").get_overlapping_bodies())
+	else:
+		pass
