10d3d1dc7991a5aa9473c194e3415ccf077a8aae
Taran Mittal <taranmittal09@gmail.com>
2026-05-30 18:00:05 +1000

    commit 2

---
+++ enemy.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -50.0
+var direction := 1
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var Player_pos = get_parent().get_node("Player").global_position.x
+	var Me_pos = self.global_position.x
+	
+	var collisions = get_node("fall").has_overlapping_bodies()
+
+	
+		
+	if ((Player_pos - Me_pos)>0):
+		direction = 1
+		
+	else: if ((Player_pos - Me_pos)<0):
+		direction = -1
+		if not collisions:
+			direction = 0
+	else:
+		direction = 0
+		
+	var jump_col = get_node("Jump").has_overlapping_bodies() and is_on_floor()
+	if jump_col:
+		velocity.y = JUMP_VELOCITY
+	else:
+		velocity.y = move_toward(velocity.y, 0, JUMP_VELOCITY)
+
+	
+
+	
+		
+	
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
